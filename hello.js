aebc9813ed46bc7e682f76c9aefe9ff8406d0088
erhic <ericngugi24@gmail.com>
2023-11-22 12:33:22 +0300

    feat:  install express package and intialize app ,add node modeles to gitignore not to be pushed to the remote repo

--- hello.js
+++ hello.js
@@ -1,18 +1,18 @@
 //load http module
-const http =require('http');
+const http = require("http");
 
-const hostname="127.0.0.1";
-const port =8000;
+const hostname = "127.0.0.1";
+const port = 8000;
 
 //create Http server
-const server= http.createServer((req,res)=>{
-//set hhtp header with http status and content type
- res.writeHead(200,{"Content-Type":"text/plain"});
- //send the responseboddyy with 'hello word'
-res.end("heeelllooworld\n")
+const server = http.createServer((req, res) => {
+  //set http header with http status and content type
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  //send the responseboddyy with 'hello word'
+  res.end("heeelllooworld\n");
 });
 
 //print a log once the server starts listening
-server.listen(port,hostname,function(){
-console.log(`Server listening at https://${hostname}:${port}/`)
-})
+server.listen(port, hostname, function () {
+  console.log(`Server listening at https://${hostname}:${port}/`);
+});
