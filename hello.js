c6806ced43083b349c219da61868a9206514edba
erhic <ericngugi24@gmail.com>
2023-11-22 11:27:16 +0300

    feat: initial commit

---
+++ hello.js
@@ -0,0 +1,18 @@
+//load http module
+const http =require('http');
+
+const hostname="127.0.0.1";
+const port =8000;
+
+//create Http server
+const server= http.createServer((req,res)=>{
+//set hhtp header with http status and content type
+ res.writeHead(200,{"Content-Type":"text/plain"});
+ //send the responseboddyy with 'hello word'
+res.end("heeelllooworld\n")
+});
+
+//print a log once the server starts listening
+server.listen(port,hostname,function(){
+console.log(`Server listening at https://${hostname}:${port}/`)
+})
