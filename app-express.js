b96aa36b622172e61a9f8c793353ed4cb56a9c6a
erhic <ericngugi24@gmail.com>
2023-11-23 12:56:27 +0300

    working with callback promises and asyn await review

--- app-express.js
+++ app-express.js
@@ -3,11 +3,11 @@ const express = require("express"); // importing express module
 const app = express(); // setting up (app variable) to be able to configure our application
 
 ///setting the port to listen to or monitor locally
-const port = 3001;
+const port = 3003;
 
 //setting up the route , takes callback function with request and response object, response obj sends /return some string.
 app.get("/", (req, res) => {
-  res.send(`hello world from express `);
+  res.send(`{"name":"Eliot","age":6787} `);
 });
 /// Stsrting server and  listening to port and logging a string with port number
 app.listen(port, () => {
--- square.js
+++ square.js
@@ -1,6 +1,7 @@
 exports.area = (width) => width * width;
 exports.perimeter = (width) => 4 * width;
 
+//working with callback
 console.log("Thursady");
 console.log("today");
 setTimeout(() => {
@@ -12,3 +13,28 @@ setTimeout(() => {
 setTimeout(() => {
   console.log("before Sunday");
 }, 4000);
+
+// working with promises
+const myPromise = (dataPoint) => {
+  return new Promise((resolve, reject) => {
+    if (successfull) {
+      resolve();
+    } else if (unsuccessful) {
+      reject();
+    }
+  });
+};
+
+//working with async await
+const asyncFunc = async () => {
+  const response = await fetch(`http://localhost:3003/`);
+  const data = await response.json();
+};
+
+console.log(1);
+console.log(2);
+
+asyncFunc().then((data) => console.log(data));
+
+console.log(3);
+console.log(4);
