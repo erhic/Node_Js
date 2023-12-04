4b81377b8c2ca50b6497636074e5125a3a9c79e0
erhic <ericngugi24@gmail.com>
2023-12-04 23:52:24 +0300

    change port

--- Node_Server/index.js
+++ Node_Server/index.js
@@ -1,10 +1,15 @@
 // Importing HTTP Modules from node .
 const http = require("http");
+const { url } = require("inspector");
 
 // creating API SERVER
 http
   .createServer((req, res) => {
-    console.log(req.status);
     console.log(res.end("end of the exuuuuuution"));
+    if (req.url === "/add") {
+      res.end("Added data");
+    } else if (req.url === "/post") {
+      res.end("update data");
+    }
   })
-  .listen(8000);
+  .liste(3000);
