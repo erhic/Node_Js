d54fc8400ac8d9c2b0b34b09b6cec0d62cb88dd0
erhic <ericngugi24@gmail.com>
2023-12-11 18:41:04 +0300

    sending request using if statements to validate

--- Node_Server/index.js
+++ Node_Server/index.js
@@ -1,15 +1,31 @@
 // Importing HTTP Modules from node, an internal module from Node  .
 const http = require("http");
+const fs = require("fs");
 const { url } = require("inspector");
 
 // creating API SERVER using a create server function with async function
+
+// Request component
+// 1. Base address/ip 192.168.1.0
+// 2. Route
+// 3. Method , GET ,POST , PATCH, DELETE
+// 4. Data
+// 5. Headers
+
 http
   .createServer((req, res) => {
-    console.log(res.end("end of the exuuuuuution"));
-    if (req.url === "/add") {
-      res.end("Added data");
-    } else if (req.url === "/post") {
-      res.end("update data");
+    if (req.url === "/add" && req.method == "POST") {
+      res.end("Added product");
+    } else if (req.url === "/user" && req.method == "POST") {
+      res.end("Added  users");
+    } else if (req.url === "/users" && req.method == "GET") {
+      fs.readFile("./products.json", "utf-8", (err, data) => {
+        if (err == null) {
+          res.end(data);
+        } else {
+          res.end("data error, cant acccess data ", err);
+        }
+      });
     }
   })
-  .listen(3012);
+  .listen(3019);
