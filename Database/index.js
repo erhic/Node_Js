00e312ea5360eecd0fe19c5564bf1f3c482c2d3e
erhic <ericngugi24@gmail.com>
2023-12-14 13:15:48 +0300

    feat: create schema ,structure for data to get inserted in db , amd a model to connect db and collection and schema

--- Database/index.js
+++ Database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+//conecting mongodb server
 mongoose
   .connect("mongodb://localhost:27017//store-products")
   .then(() => {
@@ -8,3 +9,24 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// creating the schema , the structure of data to insert to the collection (table as in sql)
+
+const prodSchema = mongoose.Schema({
+  name: String,
+  quantity: Number,
+});
+
+// creating a model , an object that gives functionality to work on the collection. This program can't work directly on the collection hence model. it keeps connection between collection and schema
+
+mongoose.model("products", prodSchema);
+
+//performing operations
+
+//dummy objects
+let product = {
+  name: "Milk",
+  quantity: 3,
+};
+
+//1. inserting objects
