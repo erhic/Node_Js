b7135c44d50df4514fe10b0e2b97d52fa1c2c526
erhic <ericngugi24@gmail.com>
2023-12-14 14:34:36 +0300

    feat : set data validation in the schema

--- Database/index.js
+++ Database/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-//conecting mongodb server
+//conecting mongodb server,add the name of database while working with local db
 mongoose
-  .connect("mongodb://localhost:27017//store-products")
+  .connect("mongodb://localhost:27017/store")
   .then(() => {
     console.log("Connection successful");
   })
@@ -13,13 +13,25 @@ mongoose
 // creating the schema , the structure of data to insert to the collection (table as in sql)
 
 const prodSchema = mongoose.Schema({
-  name: String,
-  quantity: Number,
+  name: {
+    type: String,
+    required: [true, "Name is mandatory"],
+    minLength: [3, "Minimum length is 3"],
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Quantity is mandatory"],
+    min: [3, "Minimum value is 3"],
+  },
+  category: {
+    type: [String],
+    enum: ["fruit", "dairy"],
+  },
 });
 
 // creating a model , an object that gives functionality to work on the collection. This program can't work directly on the collection hence model. it keeps connection between collection and schema
 
-mongoose.model("products", prodSchema);
+const prodModel = mongoose.model("products", prodSchema);
 
 //performing operations
 
@@ -27,6 +39,16 @@ mongoose.model("products", prodSchema);
 let product = {
   name: "Milk",
   quantity: 3,
+  category: "dairy",
 };
 
-//1. inserting objects
+//1. inserting objects , use model
+prodModel
+  .create(product)
+  .then((data) => {
+    console.log(data);
+    console.log("Data inserted succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
