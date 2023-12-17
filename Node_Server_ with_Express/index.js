69fdcdb54dde1a1ac211f01059fb6cd80b405e77
erhic <ericngugi24@gmail.com>
2023-12-17 10:48:20 +0300

    feat: create middleware

--- Node_Server_ with_Express/index.js
+++ Node_Server_ with_Express/index.js
@@ -1,2 +1,6 @@
-//imported express library
+// imported express library
 const express = require("express");
+// Creating an object of the instance
+const app = express();
+// creating a middleware to handle data .transforn our stream  data collected from chunks to json
+const mdware = express.json();
