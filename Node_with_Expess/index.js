bfa5c395c079a3a7fcb7340b34884e890af93573
erhic <ericngugi24@gmail.com>
2023-12-18 15:42:41 +0300

    feat: create app object to access method from express library or module

--- Node_with_Expess/index.js
+++ Node_with_Expess/index.js
@@ -1,4 +1,8 @@
+//Application dependecies
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+
+//Application object -to access all properties/methods
+const app = express();
