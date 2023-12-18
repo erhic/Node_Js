a495876a75ede8f2ffde763aa86c63e1c66bd61a
erhic <ericngugi24@gmail.com>
2023-12-18 15:45:17 +0300

    feat : cretae middleware to handle request body data

--- Node_with_Expess/index.js
+++ Node_with_Expess/index.js
@@ -6,3 +6,6 @@ const bcrypt = require("bcrypt");
 
 //Application object -to access all properties/methods
 const app = express();
+
+//middleware -wioll be used to handle post data or request body
+const mdware = express.json();
