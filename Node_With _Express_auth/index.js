dc193a04ef0938cc86051c1ba5b42067a11cf573
erhic <ericngugi24@gmail.com>
2023-12-19 11:42:58 +0300

    feat: create schema for data to store in db

--- Node_With _Express_auth/index.js
+++ Node_With _Express_auth/index.js
@@ -1,4 +1,29 @@
+//dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+//app
+const app = express();
+//schema
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 4,
+    },
+  },
+  { timestamps: true }
+);
+app.listen(6000, () => {
+  console.log("server running");
+});
