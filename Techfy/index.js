365fad9e1c11159a97d506998ac1b383f1ec6e5f
erhic <ericngugi24@gmail.com>
2023-12-20 11:43:16 +0300

    feat: created object to use exporess objects

--- Techfy/index.js
+++ Techfy/index.js
@@ -2,3 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+
+const app = express();
+
+app.use(express.json());
+app.listen(8500, () => {
+  console.log("server running");
+});
