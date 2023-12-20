59302fc9377767ce902cf7037613c5b0928a7bbe
erhic <ericngugi24@gmail.com>
2023-12-20 13:21:15 +0300

    refactor: register endpoint

--- Techfy/Models/userModel.js
+++ Techfy/Models/userModel.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
 //schema
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 //model
 const userModel = mongoose.model("users", userSchema);
-module.exports = { userModel };
+module.exports = userModel;
--- Techfy/index.js
+++ Techfy/index.js
@@ -18,19 +18,43 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const userModel = require("./Models/userModel");
 
 //register endpoint
 app.post("/register", (req, res) => {
-  res.send("registered");
+  // res.send("registered");
+  let user = req.body;
+  // res.send(user.password);
+
+  //hash password
+  bcrypt.genSalt(10, (err, salt) => {
+    if (!err) {
+      //if gensalt works fine  proceed to hash
+      bcrypt.hash(user.password, salt, async (err, hpass) => {
+        console.log(hpass);
+        //update hashed password before sending in db
+        user.password = hpass;
+        //send data to db now
+        try {
+          let doc = await userModel.create(user);
+          res.send({ message: "user registered" });
+        } catch {
+          (err) => {
+            console.log(err);
+          };
+        }
+      });
+    }
+  });
 });
 
 //login endpoint
 app.post("/login", (req, res) => {
-  res.send("logged in");
+  // res.send("logged in");
+  const userCredentials = req.header.authorization;
 });
-0;
 
 //app server
-app.listen(8500, () => {
+app.listen(8600, () => {
   console.log("server running");
 });
