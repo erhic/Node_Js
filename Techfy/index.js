3713b1a6b8fb67dc7cba6be4da58da34a3bf64df
erhic <ericngugi24@gmail.com>
2023-12-20 11:56:23 +0300

    feat: connecting mongodb

--- Techfy/index.js
+++ Techfy/index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
+//application
 const app = express();
-
+//middleware
 app.use(express.json());
+//db server
+mongoose
+  .connect("mongodb://localhost:27017/techfy")
+  .then(() => {
+    console.log("Db conneted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//app server
 app.listen(8500, () => {
   console.log("server running");
 });
