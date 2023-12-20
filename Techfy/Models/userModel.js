50728fa608c1bf63189c673e6d333b455689d273
erhic <ericngugi24@gmail.com>
2023-12-20 12:29:27 +0300

    feat: create user model and schema

---
+++ Techfy/Models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+//schema
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+//model
+const userModel = mongoose.model("users", userSchema);
+module.exports = { userModel };
--- Techfy/index.js
+++ Techfy/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+
 //application
 const app = express();
+
 //middleware
 app.use(express.json());
+
 //db server
 mongoose
   .connect("mongodb://localhost:27017/techfy")
@@ -15,6 +18,18 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+//register endpoint
+app.post("/register", (req, res) => {
+  res.send("registered");
+});
+
+//login endpoint
+app.post("/login", (req, res) => {
+  res.send("logged in");
+});
+0;
+
 //app server
 app.listen(8500, () => {
   console.log("server running");
